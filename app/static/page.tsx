f2539178bb5fc6e417cf227b0e85a27ff94bf9cb
Jon Meyers <jonathon.d.meyers@gmail.com>
2024-01-30 22:00:10 +1100

    add static page

---
+++ app/static/page.tsx
@@ -0,0 +1,7 @@
+export default async function Index() {
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <main className="flex-1 flex flex-col gap-6">Static</main>
+    </div>
+  );
+}
